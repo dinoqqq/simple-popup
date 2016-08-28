8d3f54e5058ce122a55ad92845ef5c7affc34481
Daan Kuijsten <daan@kuijstenwebcompany.nl>
2016-08-28 23:57:18 +0200

    added eslint to gulpfile + run it on src JS file

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
-var gulp        = require("gulp");
-var uglify      = require("gulp-uglify");
-var pump        = require("pump");
-var cleanCSS    = require("gulp-clean-css");
-var rename      = require("gulp-rename");
+const gulp          = require("gulp");
+const uglify        = require("gulp-uglify");
+const pump          = require("pump");
+const cleanCSS      = require("gulp-clean-css");
+const rename        = require("gulp-rename");
+const eslint        = require('gulp-eslint');
+const gulpIf        = require('gulp-if');
+const runSequence   = require('run-sequence');
 
-gulp.task("minify-css", function() {
-    return pump([
+function isFixed(file) {
+    // Has ESLint fixed the file contents?
+    return file.eslint != null && file.eslint.fixed;
+}
+
+gulp.task("minify-css", function(cb) {
+    pump([
         gulp.src("src/*.css"),
         cleanCSS(),
         rename({
@@ -13,19 +21,56 @@ gulp.task("minify-css", function() {
         }),
         gulp.dest("dist")
     ]);
+    cb();
 });
 
 gulp.task("compress", function (cb) {
-  return pump([
+    pump([
         gulp.src("src/*.js"),
         uglify(),
         rename({
             suffix: ".min"
         }),
-        gulp.dest("dist")
+        gulp.dest("dist"),
     ]);
+    cb()
 });
 
-gulp.task("default", ["compress", "minify-css"]);
+gulp.task("eslint", function (cb) {
+    pump([
+        gulp.src(["src/*.js"]),
+        eslint({
+            "extends": "eslint:recommended",
+            "rules": {
+                // enable additional rules
+                "indent": ["error", 4],
+                "linebreak-style": ["error", "unix"],
+                "quotes": ["error", "double"],
+                "semi": ["error", "always"],
+                "no-trailing-spaces": ["error"],
+                "valid-jsdoc": ["error"]
+            },
+            globals: [
+                'jQuery',
+                '$'
+            ],
+            envs: [
+                'browser'
+            ],
+            "fix": true
+        }),
+        eslint.format(),
+        gulpIf(isFixed, gulp.dest('src'))
+    ]);
+    cb();
+});
+
+gulp.task("default", function(done) {
+    runSequence("eslint",
+        "compress",
+        "minify-css",
+        done
+    );
+});
 
 
